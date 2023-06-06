20997a8013a03a78f0a2f3409d27622292127fe6
Gui-HR <guilhermehuertar@gmail.com>
2023-06-06 09:24:27 -0300

    Exercício Finalizado

--- aula35/exercicio.js
+++ aula35/exercicio.js
@@ -3,12 +3,12 @@ var local2 = document.querySelector('#local-vazio')
 var axolote = [...document.querySelectorAll('img')]
 var botao = document.querySelector('#botao')
 botao.addEventListener('click', mover)
-botao.addEventListener('click', voltar)
 
 axolote.map((img) => {
     img.addEventListener('click', (img) => {
         const axoloteEscolhido = img.target
         axoloteEscolhido.classList.toggle('escolhido')
+        axoloteEscolhido.classList.toggle('esse')
     })
 })
 
@@ -16,11 +16,11 @@ function mover() {
     let escolhidos = [...document.querySelectorAll('.escolhido')]
     escolhidos.map((axolote) => {
         local2.appendChild(axolote)
-        axolote.classList.add('volta')
-        remove()
-        function remove() {
-            setTimeout(10)
-            axolote.classList.remove('escolhido')
-        }
+        axolote.classList.remove('escolhido')
+    })
+
+    let voltando = [...document.querySelectorAll('#local-vazio > .esse')]
+    voltando.map((axolote) => {
+        local1.appendChild(axolote)
     })
 }
