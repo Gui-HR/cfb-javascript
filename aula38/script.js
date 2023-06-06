a00ff31b2d0649fcd47b42c395b9d3a6a512527f
Gui-HR <guilhermehuertar@gmail.com>
2023-06-06 15:40:24 -0300

    Aulas 38-40

---
+++ aula38/script.js
@@ -0,0 +1,13 @@
+var pai = document.querySelector('#container')
+
+// console.log(pai.children[2]);
+// console.log(pai.lastElementChild);
+// console.log(pai.firstElementChild);
+
+var neeko = document.querySelector('.teste')
+console.log(neeko.classList.length);
+
+if (neeko.classList.length > 1) {
+    console.log('maldito');
+}
+
---
+++ aula40/script.js
@@ -0,0 +1,6 @@
+var container = document.querySelector('#container')
+var Aphelius = document.createElement('div')
+
+container.appendChild('Aphelius')
+
+
