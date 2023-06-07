cc9353f0fed0353eb11f326ac1fce5c1682f08bc
Gui-HR <guilhermehuertar@gmail.com>
2023-06-07 11:09:45 -0300

    Aulas 40-42

--- aula41/script.js
+++ aula41/script.js
@@ -6,6 +6,14 @@ champ.map((champ) => {
     novoChamp.innerHTML = champ
     novoChamp.setAttribute('class', 'teste')
     pai.appendChild(novoChamp)
+
+    let lixo = document.createElement('div')
+    lixo.setAttribute('class', 'lixo')
+    lixo.addEventListener('click', (evento) => {
+        console.log(evento.target.parentNode);
+        pai.removeChild(evento.target.parentNode)
+    })
+    novoChamp.appendChild(lixo)
 })
 
 
