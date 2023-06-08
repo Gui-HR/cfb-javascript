6bb9d5b405016099827ac9e2550758a68fe755f8
Gui-HR <guilhermehuertar@gmail.com>
2023-06-08 12:46:05 -0300

    Aula46

---
+++ aula46/script.js
@@ -0,0 +1,53 @@
+var selecionar = document.querySelector('#selecionar')
+var remover = document.querySelector('#remover')
+var adicionar = document.querySelector('#adicionar')
+
+function selecionado() {
+    const todosAnimais = [...document.querySelectorAll('.animal')]
+    var animalSelecionado = todosAnimais.filter((animal) => {
+        return animal.checked
+    })
+    return animalSelecionado[0]
+}
+
+selecionar.addEventListener('click', () => {
+    let animalEscolhido = selecionado()
+
+    if(animalEscolhido == undefined){
+        alert('Selecione uma opção')
+    } else {
+        const animalAlerta = animalEscolhido.previousSibling.previousSibling.textContent
+        alert("O animal selecionado foi: " + animalAlerta)
+    }
+
+})
+
+remover.addEventListener('click', () => {
+    let animalEscolhido = selecionado()
+
+    if(animalEscolhido == undefined){
+        alert('Selecione uma opção')
+    } else {
+        animalEscolhido.parentNode.remove()
+    }
+})
+
+adicionar.addEventListener('click', () => {
+    const todosAnimais = [...document.querySelectorAll('animal')]
+    let nomeAnimalNovo = document.querySelector('.texto-animal-novo').value
+    if(nomeAnimalNovo == '') {
+        alert('Digite o nome do animal antes de adicionar')
+    } else {
+        if(todosAnimais.length <= 7) {
+            let animais = document.querySelector('#animais')
+            
+            let novoAnimal = document.createElement('div')
+            novoAnimal.setAttribute('class', 'radio')
+            novoAnimal.innerHTML = '<p>'+ nomeAnimalNovo +'</p>'
+    
+            animais.appendChild(novoAnimal) 
+        } else {
+            alert('Já tem muitos animais, remova um antes de criar outro.')
+        }
+    }
+})
