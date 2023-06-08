43ad43b46c423d1375e24177c183a70ad5b63eec
Gui-HR <guilhermehuertar@gmail.com>
2023-06-08 12:39:44 -0300

    Aula 43 exercício

--- aula43/script.js
+++ aula43/script.js
@@ -1,16 +1,53 @@
 var selecionar = document.querySelector('#selecionar')
+var remover = document.querySelector('#remover')
+var adicionar = document.querySelector('#adicionar')
 
 function selecionado() {
-    var animais = [...document.querySelectorAll('input[type=radio]')]
-    var animalSelecionado = animais.filter((animal) => {
+    const todosAnimais = [...document.querySelectorAll('input[type=radio]')]
+    var animalSelecionado = todosAnimais.filter((animal) => {
         return animal.checked
     })
     return animalSelecionado[0]
 }
 
 selecionar.addEventListener('click', () => {
-    var animalEscolhido= selecionado()
+    let animalEscolhido = selecionado()
 
-    const animalAlerta = animalEscolhido.previousSibling.previousSibling.textContent
-    alert("O animal selecionado foi: " + animalAlerta)
+    if(animalEscolhido == undefined){
+        alert('Selecione uma opção')
+    } else {
+        const animalAlerta = animalEscolhido.previousSibling.previousSibling.textContent
+        alert("O animal selecionado foi: " + animalAlerta)
+    }
+
+})
+
+remover.addEventListener('click', () => {
+    let animalEscolhido = selecionado()
+
+    if(animalEscolhido == undefined){
+        alert('Selecione uma opção')
+    } else {
+        animalEscolhido.parentNode.remove()
+    }
+})
+
+adicionar.addEventListener('click', () => {
+    const todosAnimais = [...document.querySelectorAll('input[type=radio]')]
+    let nomeAnimalNovo = document.querySelector('.texto-animal-novo').value
+    if(nomeAnimalNovo == '') {
+        alert('Digite o nome do animal antes de adicionar')
+    } else {
+        if(todosAnimais.length <= 7) {
+            let animais = document.querySelector('#animais')
+            
+            let novoAnimal = document.createElement('div')
+            novoAnimal.setAttribute('class', 'radio')
+            novoAnimal.innerHTML = '<p>'+ nomeAnimalNovo +'</p> <input type="radio" name="escolha">'
+    
+            animais.appendChild(novoAnimal) 
+        } else {
+            alert('Já tem muitos animais, remova um antes de criar outro.')
+        }
+    }
 })
